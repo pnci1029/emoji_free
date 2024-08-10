8a2346668581976d3ba6b43e782ad0e2a23160ba
chhong <pnci1029@gmail.com>
2024-08-10 12:05:29 +0900

    do something

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,12 +4,47 @@ export function Main() {
     return(
         <>
             <div className={style.mainWrapper}>
+                {/*  메인 헤더 영역  */}
                 <div className={style.headerWrapper}>
                     <div className={style.headerImg} />
                 </div>
 
-                <div>
+                {/*  메뉴 영역  */}
+                <div className={style.menuBoxWrapper}>
+                    <div className={style.menuButton}>
+                        <span className={style.menuText}>
+                            ALL
+                        </span>
+                    </div>
 
+                    <div className={style.menuButton}>
+                        <span className={style.menuText}>
+                            ANIMALS
+                        </span>
+                    </div>
+
+                    <div className={style.menuButton}>
+                        <span className={style.menuText}>
+                            COMPONENT
+                        </span>
+                    </div>
+
+                    <div className={`${style.menuButton} ${style.selected}`}>
+                        <span className={style.menuText}>
+                            OBJECTS
+                        </span>
+                    </div>
+
+                    <div className={style.menuButton}>
+                        <span className={style.menuText}>
+                            PEOPLE
+                        </span>
+                    </div>
+                    <div className={style.menuButton}>
+                        <span className={style.menuText}>
+                            SYMBOLS
+                        </span>
+                    </div>
                 </div>
 
             </div>
