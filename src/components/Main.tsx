ba46bdb8a87438e340acfbb23b71e05314cc093b
chhong <pnci1029@gmail.com>
2024-08-10 12:23:30 +0900

    do something

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -13,6 +13,30 @@ export function Main() {
                 {/*  메뉴 영역  */}
                 <Menu />
 
+                <div className={style.body}>
+                    <div className={style.contentsBox}>
+
+                        {/*  검색 영역  */}
+                        <div className={style.searchBox}>
+
+                            {/*  문자 검색 영역  */}
+                            <div className={style.searchTextArea}>
+                                <input
+                                    placeholder={'Search Free Emoji'}
+                                    className={style.searchInput}
+                                />
+                                <div className={style.inputImg} />
+                            </div>
+
+                            {/*  필터  */}
+                            <div className={style.filterSearchBox}>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
             </div>
         </>
     )
