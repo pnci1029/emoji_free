98c0bd772d6ba651d4853967fdfaf2a445c7fec4
chhong <pnci1029@gmail.com>
2024-08-10 11:59:14 +0900

    add some

---
+++ src/components/Main.tsx
@@ -0,0 +1,18 @@
+import style from "../assets/scss/common/main.module.scss"
+
+export function Main() {
+    return(
+        <>
+            <div className={style.mainWrapper}>
+                <div className={style.headerWrapper}>
+                    <div className={style.headerImg} />
+                </div>
+
+                <div>
+
+                </div>
+
+            </div>
+        </>
+    )
+}
